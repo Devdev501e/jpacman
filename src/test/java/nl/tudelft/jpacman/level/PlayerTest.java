97ca35edee8bc37a9fcf3a1d4287cba7456cfea1
Emie Radjouh <62715565+emie-radjouh@users.noreply.github.com>
2025-03-11 16:09:13 -0400

    test unitaire pour Player

---
+++ src/test/java/nl/tudelft/jpacman/level/PlayerTest.java
@@ -0,0 +1,32 @@
+package nl.tudelft.jpacman.level;
+
+import nl.tudelft.jpacman.sprite.PacManSprites;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests unitaires pour la classe Player.
+ */
+public class PlayerTest {
+
+    @Test
+    void playerShouldBeAliveWhenCreated() {
+        PacManSprites sprites = new PacManSprites();
+        PlayerFactory factory = new PlayerFactory(sprites);
+        Player player = factory.createPacMan();
+
+        assertThat(player.isAlive()).isTrue();
+    }
+
+    @Test
+    void playerShouldNotBeAliveAfterSetAliveFalse() {
+        PacManSprites sprites = new PacManSprites();
+        PlayerFactory factory = new PlayerFactory(sprites);
+        Player player = factory.createPacMan();
+
+        player.setAlive(false);
+
+        assertThat(player.isAlive()).isFalse();
+    }
+}
